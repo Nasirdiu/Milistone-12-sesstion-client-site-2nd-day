65cafcc385991c872b2e94c8d31a8177dd4bceba
nasirdiu <nasir.joypurhat@gmail.com>
2022-05-19 22:53:44 +0600

    first commit

---
+++ src/pages/Services.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h1>services</h1>
+            <h1>services</h1>
+            <h1>services</h1>
+            <h1>services</h1>
+            <h1>services</h1>
+            <h1>services</h1>
+            <h1>services</h1>
+            <h1>services</h1>
+            <h1>services</h1>
+            <h1>services</h1>
+        </div>
+    );
+};
+
+export default Services;
---
+++ src/routes/PublicRoutes.js
@@ -0,0 +1,9 @@
+import Contact from "../pages/Contact";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login";
+
+export const PublicRoutes = [
+  { path: "/", name: "Home", Component: Home },
+  { path: "/contact", name: "Contact", Component: Contact },
+  { path: "/login", name: "Login", Component: Login },
+];
